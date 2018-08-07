c818bc0acc15de1592e821fcb660004218c68c13
zhuhh <zhuhh@asiainfo.com>
2018-08-07 08:51:33 +0800

    测试目录

---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,7 @@
+package singleton;
+
+/**
+ * @author zhuhh 2018/8/7
+ */
+public class Singleton {
+}
---
+++ src/test/java/singleton/SingletonTest.java
@@ -0,0 +1,10 @@
+package singleton;
+
+
+/**
+ * @author zhuhh
+ */
+public class SingletonTest {
+
+
+}
